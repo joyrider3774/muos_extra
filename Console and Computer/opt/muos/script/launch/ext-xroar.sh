ca9a44bc4b85323edcaefa69bb95ce7aab13dffd
ImCoKeMaN <25371653+ImCoKeMaN@users.noreply.github.com>
2025-07-10 21:42:54 -0400

    Adding Xroar Tandy TRS-80 color computer Emulator (#2)
    * Adding Xroar Tandy TRS-80 color computer Emulator
    
    Adding support for xroar emulator for Tandy TRS-80 Color computer and dragon emulation
    bios is added to the applications/xroar/bios folder or:
    /mnt/mmc/MUOS/bios/xroar
    bas13.rom is an example of usable bios file for coco2 emulation
    
    Game files should go in a folder named coco, but xroar should be able to be assigned manually if in a different location.
    
    The launch file is:
    /opt/muos/script/launch/ext-xroar.sh
    I have edited this launcher to allow custom gptk files that match the name of the game file if placed in: MUOS/application/xroar/gptk/
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete xroar/opt/muos/script/mux directory
    
    Removed per PR comments, not needed in GOOSE
    
    * Delete xroar/mnt/mmc/MUOS/info/assign/assign.json
    
    * Update xroar.ini
    
    * Update global.ini
    
    * Update xroar.ini
    
    * moving xroar to console and computer folder

---
+++ Console and Computer/opt/muos/script/launch/ext-xroar.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+. /opt/muos/script/var/func.sh
+
+NAME=$1
+CORE=$2
+FILE=${3%/}
+
+(
+	LOG_INFO "$0" 0 "Content Launch" "DETAIL"
+	LOG_INFO "$0" 0 "NAME" "$NAME"
+	LOG_INFO "$0" 0 "CORE" "$CORE"
+	LOG_INFO "$0" 0 "FILE" "$FILE"
+) &
+
+
+GPTOKEYB="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/gptokeyb/gptokeyb2"
+XROAR_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/xroar"
+# Get the games's basename without the path
+GAME_BASENAME=$(basename "${FILE%.*}")
+# Construct the gptkfile by appending .gptk
+GPTK_FILE="${GAME_BASENAME}.gptk"
+export LD_LIBRARY_PATH="$XROAR_DIR/libs:$LD_LIBRARY_PATH"
+
+HOME="$(GET_VAR "device" "board/home")"
+export HOME
+
+SETUP_SDL_ENVIRONMENT
+
+export SDL_GAMECONTROLLERCONFIG_FILE="/usr/lib/gamecontrollerdb.txt"
+
+SET_VAR "system" "foreground_process" "xroar"
+
+
+/opt/muos/script/mux/track.sh "$NAME" "$CORE" "$FILE" start
+
+	if [ -f "$XROAR_DIR/gptk/${GPTK_FILE}" ]; then
+		$GPTOKEYB "xroar" -c "$XROAR_DIR/gptk/${GPTK_FILE}" &
+	else
+		$GPTOKEYB "xroar" -c "$XROAR_DIR/gptk/xroar.gptk" &
+	fi
+	$XROAR_DIR/xroar -c "$XROAR_DIR/xroar.conf" "$FILE"
+
+killall -q gptokeyb2
+
+/opt/muos/script/mux/track.sh "$NAME" "$CORE" "$FILE" stop
+
+unset SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
