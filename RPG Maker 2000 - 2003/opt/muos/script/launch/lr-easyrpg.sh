c84ce12cfbd82948bb1d61ae16f8f6cafb094c5a
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-09-07 21:41:26 +1000

    Updated EasyRPG Libretro launcher script

--- RPG Maker 2000 - 2003/opt/muos/script/launch/lr-easyrpg.sh
+++ RPG Maker 2000 - 2003/opt/muos/script/launch/lr-easyrpg.sh
@@ -20,31 +20,32 @@ SETUP_SDL_ENVIRONMENT
 
 SET_VAR "system" "foreground_process" "retroarch"
 
-RA_CONF="/run/muos/storage/info/config/retroarch.cfg"
-CONFIGURE_RETROARCH "$RA_CONF"
+RA_CONF="/opt/muos/share/info/config/retroarch.cfg"
+RA_ARGS=$(CONFIGURE_RETROARCH "$RA_CONF")
+
+IS_SWAP=$(DETECT_CONTROL_SWAP)
 
 /opt/muos/script/mux/track.sh "$NAME" "$CORE" "$FILE" start
 
 if [ "$(echo "$FILE" | awk -F. '{print $NF}')" = "zip" ]; then
-	nice --20 retroarch -v -f -c "$RA_CONF" -L "$(GET_VAR "device" "storage/rom/mount")/MUOS/core/$CORE" "$FILE" &
-	RA_PID=$!
-
+	nice --20 retroarch -v -f -c "$RA_CONF" $RA_ARGS -L "/opt/muos/share/core/easyrpg_libretro.so" "$FILE"
 	rm -Rf "$FILE.save"
 else
 	F_PATH=$(echo "$FILE" | awk -F'/' '{NF--; print}' OFS='/')
 	ERPC=$(sed <"$FILE.cfg" 's/[[:space:]]*$//')
 
-	if [ -d "$F_PATH/.$NAME" ]; then
-		SUBFOLDER=".$NAME"
-	else
-		SUBFOLDER="$NAME"
-	fi
+	SUB_FOLDER="$NAME"
+	[ -d "$F_PATH/.$NAME" ] && SUB_FOLDER=".$NAME"
 
-	nice --20 retroarch -v -f -c "$RA_CONF" -L "$(GET_VAR "device" "storage/rom/mount")/MUOS/core/easyrpg_libretro.so" "$F_PATH/$SUBFOLDER/$ERPC" &
-	RA_PID=$!
+	nice --20 retroarch -v -f -c "$RA_CONF" $RA_ARGS -L "/opt/muos/share/core/easyrpg_libretro.so" "$F_PATH/$SUB_FOLDER/$ERPC"
 fi
 
-wait $RA_PID
+for RF in ra_no_load ra_autoload_once.cfg; do
+	[ -e "/tmp/$RF" ] && ENSURE_REMOVED "/tmp/$RF"
+done
+
+[ "$IS_SWAP" -eq 1 ] && DETECT_CONTROL_SWAP
+
 unset SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
 
 /opt/muos/script/mux/track.sh "$NAME" "$CORE" "$FILE" stop
